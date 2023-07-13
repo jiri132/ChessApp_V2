43b9ccff1e2b81f7f6129651f86d8dddc891787b
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-13 20:09:29 +0200

    added tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,15 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+   format!("Hello, {}!", name)
+}
+
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greet])
+    .plugin(tauri_plugin_sql::Builder::default().build())
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
