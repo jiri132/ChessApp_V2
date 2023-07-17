ba30fb42f4b929b9653b0e0471737ecf1bf2de2f
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-17 19:35:52 +0200

    removed the types and interfaces

--- src/lib/DatabaseExtention/Components/SignIn.component.svelte
+++ src/lib/DatabaseExtention/Components/SignIn.component.svelte
@@ -1,89 +1,49 @@
 <script lang="ts">
     import ExtendedDatabase from "../Core/DatabaseExtention";
     import type { InputParams } from "../Core/interfaces/DatabaseExtention.TableParams.interface";
-  
-  interface FormParams {
-    [columnName: string] : {
-      type: FormInputTypes,
-      value: string | number
-    }
-  }
-  type FormInputTypes =
-  | "text"
-  | "password"
-  | "checkbox"
-  | "radio"
-  | "submit"
-  | "reset"
-  | "button"
-  | "file"
-  | "hidden"
-  | "image"
-  | "number"
-  | "range"
-  | "date"
-  | "time"
-  | "datetime-local"
-  | "month"
-  | "week"
-  | "color"
-  | "email"
-  | "url"
-  | "tel"
-  | "search"
-  | "datetime"
-  | "timestamp"
-  | "time-local"
-  | "url"
-  | "month"
-  | "week"
-  | "tel"
-  | "search"
-  | "datetime"
-  | "timestamp"
-  | "time-local";
+    import type { FormParams } from "./FormParams.interface";
+
 
   function transformFormParamsToInputParams(formParams: FormParams): InputParams {
-  const inputParams: InputParams = {};
+    const inputParams: InputParams = {};
 
-  for (const columnName in formParams) {
-    if (formParams.hasOwnProperty(columnName)) {
-      const { value } = formParams[columnName];
-      inputParams[columnName] = value;
+    for (const columnName in formParams) {
+      if (formParams.hasOwnProperty(columnName)) {
+        const { value } = formParams[columnName];
+        inputParams[columnName] = value;
+      }
     }
+
+    return inputParams;
   }
 
-  return inputParams;
-}
 
+  let data : FormParams = {
+    displayName: {type: "text", value:""},
+    userName: {type: "text", value:""},
+    email: {type: "email", value:""},
+    age: {type: "number",value:0},
+  }
 
-    let data : FormParams = {
-      displayName: {type: "text", value:""},
-      userName: {type: "text", value:""},
-      email: {type: "email", value:""},
-      age: {type: "number",value:0},
-    }
+  const db = new ExtendedDatabase(import.meta.env.VITE_URL,import.meta.env.VITE_DB)
 
-    const db = new ExtendedDatabase(import.meta.env.VITE_URL,import.meta.env.VITE_DB)
+  const handleSignUp = () => {
+    db.AppendTable('humans', transformFormParamsToInputParams(data));
+  };
 
-    console .log();
-
-    const handleSignUp = () => {
-      db.AppendTable('humans', transformFormParamsToInputParams(data));
-    };
-
-  </script>
+</script>
   
   <form>
     <h3>Sign up!</h3>
+    <div class="line"></div>
     {#each Object.keys(data) as columName,i}
       <div class="item">
-        <label>Input {columName}</label>
-        <input on:keydown={(value) => {
-            data[columName].value = value.srcElement.value; 
-            console.log(value.srcElement.value)}
+        <input on:keyup={(value) => {
+              data[columName].value = value.srcElement.value; 
+              console.log(value.srcElement.value);
+            }
           } 
-
+          placeholder="input your {columName}"
           type={data[columName].type}
         />
       </div>  
@@ -97,29 +57,37 @@
 
 <style lang="scss"> 
   form {
-    background-color: #333;
+    color: #222;
+    background-color: #dddddd;
     padding: 1rem;
     border-radius: 30px;
-    max-width: 500px;
+    max-width: 400px;
     display: flex;
     flex-direction: column;
-    row-gap: 2px;
-    .item {
-
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
+    row-gap: 5px;
+    input {
+      width: -webkit-fill-available;
+      border-radius: 100px;
+      background-color: #c0c0c0;
     }
     h3 {
+      color:#444;
       align-self: center;
       font-size: 30px;
       margin: 0px 0px 10px 0px;
     }
+    .line {
+      border: 1px rgb(255, 255, 255) solid;
+      margin-bottom: 10px;
+    }
     .action {
       flex-grow: 1;
-
+      align-self: center;
       button {
-        width: -webkit-fill-available;
+        margin-top: 10px;
+        // width: -webkit-fill-available;
+        color: white;
+        background-color:rgb(62, 92, 226);
       }
     }
   }  
