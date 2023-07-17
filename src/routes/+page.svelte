e2c3bf2eeb08c81e617ecee0781c16ea28c7d34e
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-17 17:37:52 +0200

    commented testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,40 @@
 <script lang="ts">
-  import Greet from '../lib/Greet.svelte'
-  import ExtendedDatabase from '$lib/DatabseExtention/DatabaseExtention';
-  import type { InputParams, TableParams } from '$lib/DatabseExtention/DatabaseExtention.TableParams.interface';
+  // import ExtendedDatabase from '$lib/DatabseExtention/DatabaseExtention';
+  // import type { InputParams, TableParams } from '$lib/DatabseExtention/DatabaseExtention.TableParams.interface';
 
-  const url = "mysql://user:mypassword@localhost:3306/";
-  const db = new ExtendedDatabase(url, 'testdb');
+  // const url = "mysql://user:mypassword@localhost:3306/";
+  // const db = new ExtendedDatabase(url, 'testdb');
     
-  const exampleData : TableParams = {
-    id: {
-      type: "INT(10)",
-      primaryKey: true,
-      autoIncrement: true,
-      unique: true,
-    },
-    displayName: {type:"VARCHAR(255)"},
-    userName: {
-      type:"VARCHAR(255)",
-      unique: true,
-    },
-    age: {type:"INT(3)"},
-    email: {type:"VARCHAR(255)"},
-  }
+  // const exampleData : TableParams = {
+  //   id: {
+  //     type: "INT(10)",
+  //     primaryKey: true,
+  //     autoIncrement: true,
+  //     unique: true,
+  //   },
+  //   displayName: {type:"VARCHAR(255)"},
+  //   userName: {
+  //     type:"VARCHAR(255)",
+  //     unique: true,
+  //   },
+  //   age: {type:"INT(3)"},
+  //   email: {type:"VARCHAR(255)"},
+  // }
  
-  db.CreateTable('humans', exampleData);
+  // db.CreateTable('humans', exampleData);
 
-  const exampleData2 : InputParams = {
-    displayName: "x",
-    username: "xx",
-    age: 3,
-    email: "jiri28beurskens@gmail.com"
-  };
+  // const exampleData2 : InputParams = {
+  //   displayName: "x",
+  //   username: "xx",
+  //   age: 3,
+  //   email: "jiri28beurskens@gmail.com"
+  // };
 
-  db.AppendTable('humans', exampleData2)
+  // db.AppendTable('humans', exampleData2)
 
-  db.SelectTable('humans').then((value) => {
-    console.log(value)
-  });
+  // db.SelectTable('humans').then((value) => {
+  //   console.log(value)
+  // });
 
   // const exampleData2 : InputParams = { 
   //   displayName: "HelloMyMan"
@@ -43,6 +42,11 @@
 
 
   // db.UpdateTable('humans',exampleData2, 0);
+
+  import SignIn from '$lib/DatabaseExtention/Components/SignIn.component.svelte';
+
 </script>
   
 <h1>Welcome to HSDV chess application</h1>
+
+<SignIn />
