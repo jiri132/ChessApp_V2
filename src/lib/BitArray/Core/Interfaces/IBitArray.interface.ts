0bd0e257bd1484ef3f234286d335f65a882d5653
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-18 21:41:34 +0200

    added functions

--- src/lib/BitArray/Core/Interfaces/IBitArray.interface.ts
+++ src/lib/BitArray/Core/Interfaces/IBitArray.interface.ts
@@ -1,3 +1,12 @@
+import type { BinaryDigit } from "../Types/Binary/BinaryDigit.type";
+import type { BitArrayStorage } from "../Types/BitArray.BitArrayStorage.type";
+
 export interface IBitArray {
     
+    get(index? : number) : BinaryDigit | BitArrayStorage;
+    set(value : string[]) : void;
+    set(index : number,value : BinaryDigit) : void;
+    toggle(index : number) : void;
+    clear() : void;
+    debugging() : void; 
 }
