b516324da344d17cd64900c668f18d12f5bca08f
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-19 11:05:20 +0200

    removed white space

--- src/lib/BitArray/Core/Interfaces/IBitArray.interface.ts
+++ src/lib/BitArray/Core/Interfaces/IBitArray.interface.ts
@@ -2,7 +2,6 @@ import type { BinaryDigit } from "../Types/Binary/BinaryDigit.type";
 import type { BitArrayStorage } from "../Types/BitArray.BitArrayStorage.type";
 
 export interface IBitArray {
-    
     get(index? : number) : BinaryDigit | BitArrayStorage;
     set(value : string[]) : void;
     set(index : number,value : BinaryDigit) : void;
