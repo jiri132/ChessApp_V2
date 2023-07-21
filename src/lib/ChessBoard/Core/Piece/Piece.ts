500306fced45f9137777a35bd17f544dbd1bba31
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-21 12:10:11 +0200

    updated types

--- src/lib/ChessBoard/Core/Piece/Piece.ts
+++ src/lib/ChessBoard/Core/Piece/Piece.ts
@@ -1,10 +1,13 @@
 import type ChessBoard from "../Board";
 import type { colorTable } from "./enum/Color.table.enum";
 import type { pieceTable } from "./enum/Pieces.table.enum"
+import type { move } from "../Moves/move.type"
 
 
 export interface IPiece {
     pieceData : pieceTable;
     pieceColor : colorTable;
-    legalMoves(ChessBoard : ChessBoard) : string[];            // Returns an array of legal moves @example `['f8','f7']`
+    location : move;
+    legalMoves(board : ChessBoard) : move[];            // Returns an array of legal moves @example `['f8','f7']`
+    isLegalMove(board : ChessBoard, move : string) : boolean;
 }
