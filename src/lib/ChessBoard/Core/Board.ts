3d2a7dad66ec96e052ffd425b1e0030fce65bbac
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-21 12:33:56 +0200

    created new function to get indexes

--- src/lib/ChessBoard/Core/Board.ts
+++ src/lib/ChessBoard/Core/Board.ts
@@ -8,15 +8,16 @@ import Knight from "./Piece/Pieces/Piece.Knight";
 import Bishop from "./Piece/Pieces/Piece.Bishop";
 import Queen from "./Piece/Pieces/Piece.Queen";
 import King from "./Piece/Pieces/Piece.King";
+import type { move } from "./Moves/move.type";
 
 class ChessBoard {
-    public readonly whiteBitTable : Bitboard;
-    public readonly blackBitTable : Bitboard;
-    public readonly gameBitTable  : Bitboard;
+    private readonly whiteBitTable : Bitboard;
+    private readonly blackBitTable : Bitboard;
+    private readonly gameBitTable  : Bitboard;
 
     public readonly turn : colorTable = colorTable.white; 
 
-    public readonly game : IPiece[][] = [
+    private readonly game : IPiece[][] = [
         [new Rook(colorTable.black,"A8"),new Knight(colorTable.black, "B8"), new Bishop(colorTable.black, "C8"), new Queen(colorTable.black, "D8"), new King(colorTable.black, "E7"), new Bishop(colorTable.black, "F8"),new Knight(colorTable.black, "G8"),new Rook(colorTable.black,"H8")],
         [new Pawn(colorTable.black, "A7"),new Pawn(colorTable.black, "B7"),new Pawn(colorTable.black, "C7"),new Pawn(colorTable.black, "D7"),new Pawn(colorTable.black, "E7"),new Pawn(colorTable.black, "F7"),new Pawn(colorTable.black, "G7"),new Pawn(colorTable.black, "H7")],
         [null!, null!, null!, null!, null!, null!, null!, null!],
@@ -37,24 +38,51 @@ class ChessBoard {
         this.gameBitTable = Bitboard.OR(this.whiteBitTable,this.blackBitTable);
     }
 
-    movePiece(fromPosition: string, toPosition: string): void {
+    movePiece(fromPosition: move, toPosition: move): void {
         const piece = this.getPieceAtPosition(fromPosition);
         if (!piece) {
           console.log("No piece found at the specified position.");
           return;
         }
     
-        if (piece.legalmove(this.game, toPosition)) {
-          piece.move(this.game, toPosition);
+        if (piece.isLegalMove(this, toPosition)) {
+            // Move the piece internally
+            const [fromFile, fromRank] = this.getIndexFileRank(fromPosition);
+            const [toFile, toRank] = this.getIndexFileRank(toPosition);
+
+            // Remove the piece from the original position and place it at the new position
+            this.game[toRank][toFile] = piece;
+            this.game[fromRank][fromFile] = null!;
+        
+            // Update the piece's location
+            piece.location = toPosition;
+            return;
         } else {
           console.log("Invalid move.");
+          return;
         }
     }
     
-    public getPieceAtPosition(position: string): IPiece | null {
+    public getPieceAtPosition(position: move): IPiece | null {
         // Implement logic to find and return the piece at the specified position.
         // If no piece is found, return null.
-        return null;
+        const [file, rank] = this.getIndexFileRank(position);
+
+        // Get the piece at the specified position
+        const piece = this.game[rank][file];
+
+        // Return the piece if found, otherwise return null
+        return piece ? piece : null;
+    }
+
+    private getIndexFileRank(position : move) : [number, number] {
+        const [file, rank] = position;
+
+         // Find the indices for the 'game' array based on the file and rank
+        const fileIndex = file.charCodeAt(0) - "A".charCodeAt(0);
+        const rankIndex =  8 - parseInt(rank);
+
+        return [fileIndex, rankIndex];
     }
 
 }
