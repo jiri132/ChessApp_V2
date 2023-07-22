d47b1b5f1e5108d0238899ec253d5885cebeacbf
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-22 23:03:44 +0200

    addeed legalmoves

--- src/lib/ChessBoard/Core/Piece/Pieces/Piece.Bishop.ts
+++ src/lib/ChessBoard/Core/Piece/Pieces/Piece.Bishop.ts
@@ -25,7 +25,45 @@ class Bishop implements IPiece {
         }       
     }
     legalMoves(board : ChessBoard): move[] {
-        throw new Error("Method not implemented.");
+        const [file, rank] = this.location;
+    const validMoves: move[] = [];
+
+    // All potential diagonal moves (up-right, up-left, down-right, down-left)
+    const diagonalMoves = [
+      { fileOffset: 1, rankOffset: 1 }, // Up-right
+      { fileOffset: -1, rankOffset: 1 }, // Up-left
+      { fileOffset: 1, rankOffset: -1 }, // Down-right
+      { fileOffset: -1, rankOffset: -1 }, // Down-left
+    ];
+
+    for (const moveOffset of diagonalMoves) {
+      let currentFile = file.charCodeAt(0) - "A".charCodeAt(0) + moveOffset.fileOffset;
+      let currentRank = parseInt(rank) + moveOffset.rankOffset;
+
+      while (currentFile >= 0 && currentFile <= 7 && currentRank >= 1 && currentRank <= 8) {
+        //@ts-ignore
+        const pieceAtPosition = board.getPieceAtPosition(String.fromCharCode(currentFile + 65)+ currentRank.toString());
+
+        if (!pieceAtPosition) {
+          // Empty square, add it to valid moves
+          //@ts-ignore
+          validMoves.push(String.fromCharCode(currentFile + 65)+ currentRank.toString());
+        } else {
+          if (pieceAtPosition.pieceColor !== this.pieceColor) {
+            // Enemy piece, add it to valid moves and stop further iterations in this direction
+            //@ts-ignore
+            validMoves.push(String.fromCharCode(currentFile + 65)+ currentRank.toString());
+          }
+          break; // Stop further iterations in this direction, as we can't move through pieces
+        }
+
+        // Move to the next square in the same diagonal direction
+        currentFile += moveOffset.fileOffset;
+        currentRank += moveOffset.rankOffset;
+      }
+    }
+
+    return validMoves;
     }
 
 }
