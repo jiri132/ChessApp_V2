eb2f70a94c93b89be3d1c5f5347db4b4fb3fb1dd
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-24 19:42:22 +0200

    updated that it doenst walk into checks

--- src/lib/ChessBoard/Core/Piece/Pieces/Piece.King.ts
+++ src/lib/ChessBoard/Core/Piece/Pieces/Piece.King.ts
@@ -32,30 +32,36 @@ class King implements IPiece {
 
         // All potential moves (up, down, left, right, and the four diagonal directions)
         const allMoves = [
-        { fileOffset: 1, rankOffset: 0 }, // Right
-        { fileOffset: -1, rankOffset: 0 }, // Left
-        { fileOffset: 0, rankOffset: 1 }, // Up
-        { fileOffset: 0, rankOffset: -1 }, // Down
-        { fileOffset: 1, rankOffset: 1 }, // Up-right
-        { fileOffset: -1, rankOffset: 1 }, // Up-left
-        { fileOffset: 1, rankOffset: -1 }, // Down-right
-        { fileOffset: -1, rankOffset: -1 }, // Down-left
+            { fileOffset: 1, rankOffset: 0 }, // Right
+            { fileOffset: -1, rankOffset: 0 }, // Left
+            { fileOffset: 0, rankOffset: 1 }, // Up
+            { fileOffset: 0, rankOffset: -1 }, // Down
+            { fileOffset: 1, rankOffset: 1 }, // Up-right
+            { fileOffset: -1, rankOffset: 1 }, // Up-left
+            { fileOffset: 1, rankOffset: -1 }, // Down-right
+            { fileOffset: -1, rankOffset: -1 }, // Down-left
         ];
 
         for (const moveOffset of allMoves) {
-        const currentFile = file.charCodeAt(0) - "A".charCodeAt(0) + moveOffset.fileOffset;
-        const currentRank = parseInt(rank) + moveOffset.rankOffset;
+            const currentFile = file.charCodeAt(0) - "A".charCodeAt(0) + moveOffset.fileOffset;
+            const currentRank = parseInt(rank) + moveOffset.rankOffset;
 
-        if (currentFile >= 0 && currentFile <= 7 && currentRank >= 1 && currentRank <= 8) {
-            // @ts-ignore
-            const pieceAtPosition = board.getPieceAtPosition(String.fromCharCode(currentFile + 65) + currentRank.toString());
+            if (currentFile >= 0 && currentFile <= 7 && currentRank >= 1 && currentRank <= 8) {
+                // @ts-ignore
+                const tile : move = String.fromCharCode(currentFile + 65) + currentRank.toString();
+                const pieceAtPosition = board.getPieceAtPosition(tile);
 
-            if (!pieceAtPosition || pieceAtPosition.pieceColor !== this.pieceColor) {
-            // Empty square or enemy piece, add it to valid moves
-            // @ts-ignore
-            validMoves.push(String.fromCharCode(currentFile + 65) + currentRank.toString());
+                if (pieceAtPosition === null) {continue;}
+
+                // console.log(tile)
+                if (board.isAttackedSquare(tile)) {continue;}
+
+                if (!pieceAtPosition || pieceAtPosition.pieceColor !== this.pieceColor ) {
+                    // Empty square or enemy piece, add it to valid moves
+                    // @ts-ignore
+                    validMoves.push(String.fromCharCode(currentFile + 65) + currentRank.toString());
+                }
             }
-        }
         }
 
         // Check for castling opportunities (Kingside and Queenside)
