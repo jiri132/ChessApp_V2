8ec4f2ce13ac57bef80f168af107f56ec82c99c5
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-24 14:42:47 +0200

    REWORK
    there is going to be a big rework after a full night of engineering the behaviour of this class

--- src/lib/ChessBoard/Core/Board.ts
+++ src/lib/ChessBoard/Core/Board.ts
@@ -1,6 +1,6 @@
-import { pieceTable } from "./Piece/enum/Pieces.table.enum";
+// import { pieceTable } from "./Piece/enum/Pieces.table.enum";
 import { colorTable } from "./Piece/enum/Color.table.enum";
-import Bitboard from "$lib/BitArray/Extensions/BitBoard";
+// import Bitboard from "$lib/BitArray/Extensions/BitBoard";
 import type {IPiece} from "./Piece/Piece";
 import Rook from "./Piece/Pieces/Piece.Rook";
 import Pawn from "./Piece/Pieces/Piece.Pawn";
@@ -9,15 +9,19 @@ import Bishop from "./Piece/Pieces/Piece.Bishop";
 import Queen from "./Piece/Pieces/Piece.Queen";
 import King from "./Piece/Pieces/Piece.King";
 import type { move } from "./Moves/move.type";
+import type { IBoard } from "./IBoard.interface";
+import type { playedMoves } from "./Moves/playedMoves.type";
 
-class ChessBoard {
-    private readonly whiteBitTable : Bitboard;
-    private readonly blackBitTable : Bitboard;
-    private readonly gameBitTable  : Bitboard;
+class ChessBoard implements IBoard {
+    // private readonly whiteBitTable : Bitboard;
+    // private readonly blackBitTable : Bitboard;
+    // private readonly gameBitTable  : Bitboard;
 
-    public readonly turn : colorTable = colorTable.white; 
+    public readonly playedMoves : playedMoves[][] = [];
 
-    private readonly game : IPiece[][] = [
+
+    // private readonly turn : colorTable = colorTable.white; 
+    public readonly game : IPiece[][] = [
         [new Rook(colorTable.black,"A8"),new Knight(colorTable.black, "B8"), new Bishop(colorTable.black, "C8"), new Queen(colorTable.black, "D8"), new King(colorTable.black, "E7"), new Bishop(colorTable.black, "F8"),new Knight(colorTable.black, "G8"),new Rook(colorTable.black,"H8")],
         [new Pawn(colorTable.black, "A7"),new Pawn(colorTable.black, "B7"),new Pawn(colorTable.black, "C7"),new Pawn(colorTable.black, "D7"),new Pawn(colorTable.black, "E7"),new Pawn(colorTable.black, "F7"),new Pawn(colorTable.black, "G7"),new Pawn(colorTable.black, "H7")],
         [null!, null!, null!, null!, null!, null!, null!, null!],
@@ -28,40 +32,16 @@ class ChessBoard {
         [new Rook(colorTable.white,"A1"), new Knight(colorTable.white,"B1"), new Bishop(colorTable.white,"C1"), new Queen(colorTable.white, "D1"), new King(colorTable.white, "E1"), new Bishop(colorTable.white, "F1"),new Knight(colorTable.white, "G1"),new Rook(colorTable.white,"H1")]
     ]
 
-
-    constructor(
-        whiteBitBoard : Bitboard = new Bitboard(64,["00000000","00000000","00000000","00000000","00000000","00000000","11111111","11111111"]),
-        blackBitBoard : Bitboard = new Bitboard(64,["11111111","11111111","00000000","00000000","00000000","00000000","00000000","00000000"]))
-        {
-        this.whiteBitTable = whiteBitBoard;
-        this.blackBitTable = blackBitBoard;
-        this.gameBitTable = Bitboard.OR(this.whiteBitTable,this.blackBitTable);
+    makeMove(): void {
+        throw new Error("Method not implemented.");
     }
-
-    movePiece(fromPosition: move, toPosition: move): void {
-        const piece = this.getPieceAtPosition(fromPosition);
-        if (!piece) {
-          console.log("No piece found at the specified position.");
-          return;
-        }
+    undoMove() : void {
+        throw new Error("Method not implemented.");
+    }
+    playMove(): void {
+        throw new Error("Method not implemented.");        
+    }
     
-        if (piece.isLegalMove(this, toPosition)) {
-            // Move the piece internally
-            const [fromFile, fromRank] = this.getIndexFileRank(fromPosition);
-            const [toFile, toRank] = this.getIndexFileRank(toPosition);
-
-            // Remove the piece from the original position and place it at the new position
-            this.game[toRank][toFile] = piece;
-            this.game[fromRank][fromFile] = null!;
-            console.log(this.game)
-            // Update the piece's location
-            piece.location = toPosition;
-            return;
-        } else {
-          console.log("Invalid move.");
-          return;
-        }
-    }
     
     public getPieceAtPosition(position: move): IPiece | null {
         // Implement logic to find and return the piece at the specified position.
@@ -75,6 +55,88 @@ class ChessBoard {
         return piece ? piece : null;
     }
 
+    public getAllLegalMoves() : playedMoves[] {
+        const allMoves : playedMoves[] = [];
+        console.log(this.game)
+
+        for (let i = 0; i < this.game.length; i++) {
+            const rank = this.game[i];
+                
+            // 0. evade the null!
+            // 1. Check if the piece is a specific color
+            // 2. Move on to getting the legal moves
+            // 3. Return the legal moves into an array
+            rank.forEach((piece : IPiece, index : number) => {
+                const file = index;    
+
+                if (piece === null) {return;}
+
+
+                if (this.isWhiteTurn()  && piece.pieceColor === "0") {
+                    let moves : move[] = piece.legalMoves(this);
+                    
+
+                } else if (!this.isWhiteTurn()  && piece.pieceColor === "1"){
+
+                }
+
+            });
+            
+        }
+        return null!;
+    }
+
+    public isWhiteTurn() : boolean {
+        const whiteLatestMove = this.playedMoves[this.playedMoves.length-1][0];
+
+        if (whiteLatestMove === undefined) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    /*
+    *
+    *   **PRIVATES**
+    * 
+    *   These functions help the repeating behaviors of the board
+    *
+    */
+
+    // private movePiece(fromPosition : move, toPosition : move) : IPiece[][] {
+    //     // Get piece from the virtual board
+    //     const piece = this.getPieceAtPosition(fromPosition);
+
+    //     // error if the virtual board doesn't have a piece there
+    //     if (!piece) {
+    //         throw new Error("No piece found at the specified position");
+    //     }
+
+
+    //     if (piece.isLegalMove(this, toPosition)) {
+    //         // Move the piece internally
+    //         const [fromFile, fromRank] = this.getIndexFileRank(fromPosition);
+    //         const [toFile, toRank] = this.getIndexFileRank(toPosition);
+
+    //         // Remove the piece from the original position and place it at the new position
+    //         this.game[toRank][toFile] = piece;
+    //         this.game[fromRank][fromFile] = null!;
+    //         // console.log(board.game)
+
+    //         // Update the piece's location
+    //         piece.location = toPosition;
+            
+    //         // return the new positions
+    //         return this.game;
+    //     } else {
+    //         // If move is invalid return null
+    //         throw new Error("Invalid Move");
+    //     }
+    // }
+
     private getIndexFileRank(position : move) : [number, number] {
         const [file, rank] = position;
 
@@ -84,6 +146,8 @@ class ChessBoard {
 
         return [fileIndex, rankIndex];
     }
+
+
 
 }
 
