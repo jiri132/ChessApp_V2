72030427e9a2067e1b857d559283828acb33dac8
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-26 16:30:43 +0200

    changed it to youre editable bot

--- src/lib/ChessEngine/myBot.ts
+++ src/lib/ChessEngine/myBot.ts
@@ -1,5 +1,4 @@
 import Chess_API_Bots from "$lib/ChessBoard/API/Interactions/Board.API.bot";
-import type { move } from "$lib/ChessBoard/Core/Moves/move.type";
 import type { playedMoves } from "$lib/ChessBoard/Core/Moves/playedMoves.type";
 import type { ImyBot } from "./ImyBot";
 
@@ -7,35 +6,17 @@ import type { ImyBot } from "./ImyBot";
 
 class myBot extends Chess_API_Bots implements ImyBot {
     
-    readonly values : number[] = [100,300,300,500,900,10000]
-
     Think(): playedMoves {
+        // Variable that gets given back
         let playingMove : playedMoves;
 
+        // Get a random number between 0 and the possibler moves
+        let randomNum = getRandomInteger(0, this.API.getYourLegalMoves().length)
+        
+        // Set the playing move to the random selected move
+        playingMove = this.API.getYourLegalMoves()[randomNum];
 
-        let randomNum = getRandomInteger(0,this.API.getYourLegalMoves().length);
-            playingMove = this.API.getYourLegalMoves()[randomNum];
-
-        let capturingMove : playedMoves | undefined = undefined;
-        let captureValue : number = 0; 
-
-        this.API.getYourLegalMoves().forEach((move : playedMoves) => {
-            const from : move = move.substring(0,2) as move;
-            const to : move = move.substring(2,4) as move;
-
-            let value : number = this.values[parseInt(this.API.getPieceAtPosition(to)?.pieceData as string,2)];
-
-            if (value > captureValue) {
-                capturingMove  = (from+to).toString() as playedMoves;
-                captureValue = value;
-            }
-        })
-
-        if ( capturingMove !== undefined) {
-            playingMove = capturingMove;
-        }
-
-
+        // Play the selected move
         return playingMove;
         
     }
