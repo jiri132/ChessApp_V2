5640299eaef887aa3fadae65f6accf84d6f8d01d
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-26 16:31:21 +0200

    added styling and moves UI + playingType UI

--- src/lib/ChessBoard/UI-Frameworks/board.svelte
+++ src/lib/ChessBoard/UI-Frameworks/board.svelte
@@ -6,25 +6,27 @@
     import type { IPiece } from "../Core/Piece/IPiece";
     import { colorTable } from "../Core/Piece/enum/Color.table.enum";
     import Chess_API_Visuals from "../API/Visuals/Board.API.Visuals";
+    import { playStyles } from "../Core/PlayStyles/Board.PlayStyles.enum";
 
-    const Board = new ChessBoard();
-    const API_visuals = new Chess_API_Visuals(Board);
-
+    
     let movingPiece : IPiece;
     let possibleMoves : move[] = [];
+    let playstyle : playStyles = playStyles.Human_vs_MyBot;
+
+    let Board : ChessBoard = new ChessBoard(playstyle);
 
     function SelectedOwnPiece(piece : IPiece) : boolean {
         if ((piece.pieceColor === colorTable.white && Board.isWhiteToMove) || 
             (piece.pieceColor === colorTable.black && !Board.isWhiteToMove)) {
             // Remove coloring
-            API_visuals.removeHighlightsPossibleMoves(possibleMoves);
+            Chess_API_Visuals.removeHighlightsPossibleMoves(possibleMoves);
 
             // Assign pieces and moves
             movingPiece = piece; 
             possibleMoves = piece.legalMoves(Board);
 
             // Highlight possible moves again
-            API_visuals.highlightPossibleMoves(possibleMoves);
+            Chess_API_Visuals.highlightPossibleMoves(possibleMoves);
 
             return true;
         }
@@ -42,7 +44,7 @@
         Board.playMove(playingMove);
 
         // Render Squares 
-        API_visuals.RenderPlayedMove(playingMove);
+        Chess_API_Visuals.RenderPlayedMove(playingMove, Board);
     }
 
 
@@ -65,7 +67,7 @@
             movingPiece = piece;
             possibleMoves = piece.legalMoves(Board);
 
-            API_visuals.highlightPossibleMoves(possibleMoves);
+            Chess_API_Visuals.highlightPossibleMoves(possibleMoves);
         }else {
             movePiece(id);
             
@@ -73,20 +75,30 @@
         }
     }
     function reset() {
-        API_visuals.removeHighlightsPossibleMoves(possibleMoves);
+        Chess_API_Visuals.removeHighlightsPossibleMoves(possibleMoves);
 
         movingPiece = null!;
         possibleMoves = [];
     }
-    
+    function setPlayStyle(newPlayStyle : playStyles) {
+        playstyle = newPlayStyle;
+        Board = new ChessBoard(playstyle);
+        Chess_API_Visuals.RenderAllSquares(Board);
+    }
     
     onMount(() => {
-        API_visuals.RenderAllSquares();
+        Board = new ChessBoard(playstyle);
+        Chess_API_Visuals.RenderAllSquares(Board);
     }) 
 </script>
 
-<div>
-    <div class="container">
+<div class="visualContainer">
+    <div class="infoStack">
+        <button on:click={(e) => {setPlayStyle(playStyles.Human_vs_MyBot);}}>Human vs MyBot</button>
+        <button on:click={(e) => {setPlayStyle(playStyles.MyBot_vs_MyBot);}}>MyBot vs MyBot</button>
+        <button on:click={(e) => {setPlayStyle(playStyles.MyBot_vs_DevBot);}}>MyBot vs DevBot</button>
+    </div>
+    <div class="ChessBoard">
         {#each Board.game as row, file}
                 {#each Board.game[file] as piece, rank}
                     <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -100,25 +112,70 @@
                 {/each}
         {/each}
     </div>
+    <div class="infoStack">
+        <div style="color: black;">Played Moves</div>
+        <div id="moves" class="moves">
+
+        </div>
+    </div>
 </div>
 <style lang="scss">
-    .container {
-		display: grid;
-		grid-template-rows: repeat(8, 0fr);
-		grid-template-columns: repeat(8, 0fr);
-        max-width: calc(60px * 8);
-        max-height: calc(60px * 8);
-        background-image: url("/src/lib/assets/board.png");
-        background-size: calc(60px * 8);
-        background-repeat: no-repeat;
-	}
-    .card {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-        color: darkblue;
-		background-color: rgb(173, 216, 230,0);
-		width: 60px;
-        height: 60px;
-	}
+    
+    .visualContainer {
+        background-color: white;
+        display: flex;
+        flex-direction: row;
+        column-gap: 10px;
+        padding: 5px;
+        border-radius: 10px;
+        width: 100%;
+        max-width: 900px;
+        .infoStack {
+            width: 100%;
+            border-radius: 10px;
+            background-color: rgb(197, 197, 197);
+            display: flex;
+            flex-direction: column;
+            row-gap: 10px;
+            max-height: 480px;
+            padding: 5px;
+            text-align: center;
+        }
+        
+        .moves {
+            color: black;
+            overflow-y: scroll;
+            display: grid;
+            grid-template-rows: repeat(2, 0fr);
+            grid-template-columns: repeat(3,1fr);
+            justify-items: center;
+        }
+        .moves::-webkit-scrollbar {
+            width: 0px;
+        }
+
+        .ChessBoard {
+            display: grid;
+            grid-template-rows: repeat(8, 0fr);
+            grid-template-columns: repeat(8, 0fr);
+            max-width: calc(60px * 8);
+            max-height: calc(60px * 8);
+            background-image: url("/src/lib/assets/board.png");
+            background-size: calc(60px * 8);
+            background-repeat: no-repeat;
+
+            .card {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                color: darkblue;
+                background-color: rgb(173, 216, 230,0);
+                width: 60px;
+                height: 60px;
+            }
+	    }
+    }
+    
+
+    
 </style>
