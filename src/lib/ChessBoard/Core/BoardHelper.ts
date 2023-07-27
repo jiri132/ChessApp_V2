445f3208296a12e3c21c50a64eb4acf93d069ec3
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-27 22:40:43 +0200

    finished isInCheck()

--- src/lib/ChessBoard/Core/BoardHelper.ts
+++ src/lib/ChessBoard/Core/BoardHelper.ts
@@ -1,4 +1,6 @@
 import type ChessBoard from "./Board";
+import type { move } from "./Moves/move.type";
+import type { playedMoves } from "./Moves/playedMoves.type";
 import type { IPiece } from "./Piece/IPiece";
 import { colorTable } from "./Piece/enum/Color.table.enum";
 import { pieceTable } from "./Piece/enum/Pieces.table.enum";
@@ -15,7 +17,24 @@ class BoardHelper {
             return piece?.pieceData === pieceTable.King && piece?.pieceColor === colorTable.black;
         }) as IPiece;
 
-        // console.log(king);
+        // When the king is on an attacked square return it as a check
+        if (board.isAttackedSquare(king.location)) {return true;}
+
+        // else it is probably not in check so return false
+        return false;
+    }
+
+    static isNextMoveCheck(board : ChessBoard, move : playedMoves) : boolean {
+        const king : IPiece = board.game.flat().find((piece) => {
+            if (board.isWhiteToMove) {
+                return piece?.pieceData === pieceTable.King && piece?.pieceColor === colorTable.white;
+            }
+
+            return piece?.pieceData === pieceTable.King && piece?.pieceColor === colorTable.black;
+        }) as IPiece;
+
+        
+
 
         return false;
     }
