d5e22072d3d45f49588055c63ed084901847db54
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-28 17:32:56 +0200

    Chenged API's folderlayout to using core versions

--- src/lib/ChessBoard/API/[V1]/Interactions/Board.API.bot.ts
+++ src/lib/ChessBoard/API/[V1]/Interactions/Board.API.bot.ts
@@ -1,4 +1,4 @@
-import type ChessBoard from "$lib/ChessBoard/Core/Board";
+import type ChessBoard from "$lib/ChessBoard/Core/[V1] DepricatedCore/Board";
 import type { playedMoves } from "$lib/ChessBoard/Core/[V1] DepricatedCore/Moves/playedMoves.type";
 import type Chess_API_Visuals from "../Visuals/Board.API.Visuals";
 
