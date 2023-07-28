44d76b28285efd0d7c06206959fb99e8477e0dd1
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-28 20:30:46 +0200

    Board now created cleasr chessboard

--- src/lib/ChessBoard/Core/[V2]/Board/Board.ts
+++ src/lib/ChessBoard/Core/[V2]/Board/Board.ts
@@ -1,18 +1,26 @@
+import type { BinaryDigit } from "$lib/BitArray/Core/Types/Binary/BinaryDigit.type";
+import BoardHelper from "../Helpers/BoardHelper";
 import type { IBoard } from "../Interfaces/Board/IBoard";
+import type { IPiece } from "../Interfaces/Board/Pieces/IPieces";
 import type Move from "../Move/Move";
-import type Tile from "./Tile/Tile";
+import Bishop from "./Pieces/Piece.bishop";
+import King from "./Pieces/Piece.king";
+import Knight from "./Pieces/Piece.knight";
+import Pawn from "./Pieces/Piece.pawn";
+import Queen from "./Pieces/Piece.queen";
+import Rook from "./Pieces/Piece.rook";
+import Tile from "./Tile/Tile";
 
 class Board implements IBoard {
     public readonly tiles: Tile[] = [];
     public readonly playedMoves: Move[] = new Proxy<Move[]>([], {
         set: (target: Move[], property : string, value: number) => {
             if (property === "length") {
-                if (value === 0) {return Reflect.set(target, property, 0);}
                 // Handle length property change (e.g., push)
                 // You can also add further checks if needed
         
                 // Call the RenderPlayedMove function passing the value and the current ChessBoard API
-                // TODO: Change it from Depricated visual API to BoardVisualHelper.
+                // TODO: Change it from Deprecated visual API to BoardVisualHelper.
                 //Chess_API_Visuals.RenderPlayedMoves(this);
                 //Chess_API_Visuals.RenderPlayedMove(target[value-1], this);
             }
@@ -23,7 +31,56 @@ class Board implements IBoard {
     }) as Move[]; // Cast the proxy to the desired type (playedMoves[]);
     
     constructor() {
+        const width : number = 8;
+        const height : number = 8;
 
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                const color : BinaryDigit = (i === 1 || i === 2) ? "0" : "1";
+                const index : number = i * j;
+                const BoardLocation = BoardHelper.indexToBoardLocation(index);
+                
+                let piece : IPiece | undefined;
+
+                // Spawn the special row pieces
+                if (i === 0 || i === 7) {
+                    switch(j) {
+                        case 0:
+                            piece = new Rook(BoardLocation,color);
+                            break;
+                        case 1:
+                            piece = new Knight(BoardLocation,color);
+                            break;
+                        case 2:
+                            piece = new Bishop(BoardLocation,color);
+                            break;
+                        case 3:
+                            piece = new Queen(BoardLocation,color);
+                            break;
+                        case 4:
+                            piece = new King(BoardLocation,color);
+                            break;
+                        case 5:
+                            piece = new Bishop(BoardLocation,color);
+                            break;
+                        case 6:
+                            piece = new Knight(BoardLocation,color);
+                            break;
+                        case 7:
+                            piece = new Rook(BoardLocation,color);
+                            break;
+                    }
+                }
+                // Spawn the pawns on spot 2 or 7
+                if (i === 1 || i === 6) {
+                    piece = new Pawn(BoardLocation, color);
+                }
+
+                const tile : Tile = new Tile(BoardLocation)
+
+                this.tiles.push(tile);
+            }
+        }
     }    
 }
 
