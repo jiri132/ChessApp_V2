c544905ad5a0eba64af423f1bb9c0957836cda9d
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-28 21:07:40 +0200

    added constructor for chessboard

--- src/lib/ChessBoard/Core/[V2]/ChessBoard.ts
+++ src/lib/ChessBoard/Core/[V2]/ChessBoard.ts
@@ -1,9 +1,12 @@
-import type Board from "./Board/Board";
+import Board from "./Board/Board";
 import type { IChessBoard } from "./Interfaces/IChessBoard";
 
 class ChessBoard implements IChessBoard {
     Board: Board;
 
+    constructor() {
+        this.Board = new Board();
+    }
 }
 
 export default ChessBoard;
