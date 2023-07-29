8293ff5d40c7e34b94f6f3400665097d48c95ba6
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-29 11:51:51 +0200

    created a constructor
    may get removed because wanting to have it in JSON format so maybe gets transfered to object later in the project

--- src/lib/ChessBoard/Core/[V2]/PlayedGames/PlayedGames.ts
+++ src/lib/ChessBoard/Core/[V2]/PlayedGames/PlayedGames.ts
@@ -7,6 +7,15 @@ class PlayedGames implements IPlayedGames {
     public readonly Game_WhitePlayer: undefined;
     public readonly Game_BlackPlayer: undefined;
     public readonly Game_Outcome: undefined;
+
+    constructor(PlayedMoves : Move[], ID  :number, whitePlayer? : undefined, blackPlayer? : undefined, outcome? :undefined) {
+        this.Game_PlayedMoves = PlayedMoves;
+        this.Game_ID = ID;
+        
+        this.Game_WhitePlayer = whitePlayer;
+        this.Game_BlackPlayer = blackPlayer;
+        this.Game_Outcome = outcome;
+    }
 }
 
 export default PlayedGames;
