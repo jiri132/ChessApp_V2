84397708b95c271ac671696a9ca07ca7667cf5a2
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-29 11:50:52 +0200

    created new function to start games

--- src/lib/ChessBoard/Core/[V2]/ChessBoard.ts
+++ src/lib/ChessBoard/Core/[V2]/ChessBoard.ts
@@ -1,10 +1,32 @@
+import { bounceIn } from "svelte/easing";
 import Board from "./Board/Board";
 import type { IChessBoard } from "./Interfaces/IChessBoard";
+import PlayedGames from "./PlayedGames/PlayedGames";
 
 class ChessBoard implements IChessBoard {
-    Board: Board;
+
+
+    public Board: Board = new Board();
+
+
+    public readonly PlayedGames : PlayedGames[] = [];
+
+
 
     constructor() {
+        this.newGame();
+    }
+
+    public newGame() {
+        if (this.Board.playedMoves.length > 0) {
+            // create a played games object
+            const playedGame : PlayedGames = new PlayedGames(this.Board.playedMoves,PlayedGames.length);
+
+            //put everything into the list
+            this.PlayedGames.push(playedGame);
+        }
+
+        // Create new board
         this.Board = new Board();
     }
 }
--- src/lib/ChessBoard/Core/[V2]/Interfaces/IChessBoard.ts
+++ src/lib/ChessBoard/Core/[V2]/Interfaces/IChessBoard.ts
@@ -1,9 +1,12 @@
 import type Board from "../Board/Board";
+import type PlayedGames from "../PlayedGames/PlayedGames";
 
 export interface IChessBoard {
     /*  This is the main board where everything is on played  */
     Board : Board;
 
     /*  Save all the played games  */
-    //readonly PlayedGames : [][] //TODO: make a type for playedGames content: {moves[], ID, white,black, win state}
+    readonly PlayedGames : PlayedGames[]
+
+    newGame() : void;
 }
