670fa27dc2a4f48de55ff1689069bdef12b3a9b4
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-31 16:53:32 +0200

    added pawn moves

--- src/lib/ChessBoard/Core/[V2]/Board/Pieces/Piece.pawn.ts
+++ src/lib/ChessBoard/Core/[V2]/Board/Pieces/Piece.pawn.ts
@@ -1,8 +1,10 @@
 import type { BinaryDigit } from "$lib/BitArray/Core/Types/Binary/BinaryDigit.type";
 import type { BinaryGroup } from "$lib/BitArray/Core/Types/Binary/BinaryGroup.type";
+import BoardHelper from "../../Helpers/BoardHelper";
 import type { IPiece } from "../../Interfaces/Board/Pieces/IPieces";
-import type Move from "../../Move/Move";
+import Move from "../../Move/Move";
 import type { BoardLocation } from "../../Types/Location/Location.type";
+import type Board from "../Board";
 
 class Pawn implements IPiece {
     public location : BoardLocation;
@@ -17,8 +19,63 @@ class Pawn implements IPiece {
         this.piece = "000";
         this.data = (this.color + this.piece) as BinaryGroup<4>;
     }
-    public PieceLegalMoves(): Move[] {
-        throw new Error("Method not implemented.");
+    public PieceLegalMoves(API : Board): Move[] {
+        const [file, rank] = this.location;
+        const legalMoves: Move[] = [];
+
+        // Determine the direction the pawn should move based on its color
+        const direction = this.color === "0" ? 1 : -1;
+
+        // Check the single square in front of the pawn
+        const nextRank = parseInt(rank) + direction;
+        const nextSquare : BoardLocation = `${file}${nextRank}` as BoardLocation;
+        // return empty array when the next rank is outside of the area
+        if (nextRank === 0 || nextRank > 8) {
+            return [];
+        }
+
+        if (!BoardHelper.findPieceAtLocation(API,nextSquare)) {
+            // TODO: Update to the `new Move();`
+            //legalMoves.push(nextSquare);
+        }
+
+        // Check the double square move from the starting position
+        if ((this.color === "0" && rank === "2") || (this.color === "1" && rank === "7")) {
+            const doubleMoveRank = String.fromCharCode(rank.charCodeAt(0) + 2 * direction);
+
+            const doubleMoveSquare : BoardLocation = `${file}${doubleMoveRank}` as BoardLocation;
+            if (!BoardHelper.findPieceAtLocation(API,doubleMoveSquare) && !BoardHelper.findPieceAtLocation(API, nextSquare)) {
+                // TODO: Update to the `new Move();`
+                //legalMoves.push(doubleMoveSquare);
+            }
+        }
+
+
+        // Check captures diagonally
+        const captureLocations: BoardLocation[] = [
+            String.fromCharCode(file.charCodeAt(0) + 1) + nextRank as BoardLocation, 
+            String.fromCharCode(file.charCodeAt(0) - 1) + nextRank as BoardLocation,
+        ];
+
+        for (const move of captureLocations) {
+            if (move.charAt(0) === "@" || move.charAt(0) === "I") {
+                continue;
+            }
+
+            const _move : Move = new Move(this,move,API);
+
+            // Push when it is not undefined
+            if (_move.capturedPiece === undefined) { 
+                legalMoves.push(_move);
+                break; 
+            }
+            // break if same color
+            if (_move.capturedPiece.color === this.color) { break; }   
+            
+            legalMoves.push(_move);
+        }
+
+        return legalMoves;
     }
 }
 
