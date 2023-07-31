d0168342f3677e837d4c94af55674afffc83649c
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-07-31 16:53:26 +0200

    added knight moves

--- src/lib/ChessBoard/Core/[V2]/Board/Pieces/Piece.knight.ts
+++ src/lib/ChessBoard/Core/[V2]/Board/Pieces/Piece.knight.ts
@@ -1,8 +1,10 @@
 import type { BinaryDigit } from "$lib/BitArray/Core/Types/Binary/BinaryDigit.type";
 import type { BinaryGroup } from "$lib/BitArray/Core/Types/Binary/BinaryGroup.type";
+import BoardHelper from "../../Helpers/BoardHelper";
 import type { IPiece } from "../../Interfaces/Board/Pieces/IPieces";
-import type Move from "../../Move/Move";
+import Move from "../../Move/Move";
 import type { BoardLocation } from "../../Types/Location/Location.type";
+import type Board from "../Board";
 
 class Knight implements IPiece {
     public location : BoardLocation;
@@ -18,8 +20,39 @@ class Knight implements IPiece {
         this.data = (this.color + this.piece) as BinaryGroup<4>;
     }
 
-    public PieceLegalMoves(): Move[] {
-        throw new Error("Method not implemented.");
+    public PieceLegalMoves(API : Board): Move[] {
+        const [file, rank] = this.location;
+        const validMoves: Move[] = [];
+    
+        // All potential knight moves from the current position
+        const knightMoves = [
+          { fileOffset: 1, rankOffset: 2 },
+          { fileOffset: -1, rankOffset: 2 },
+          { fileOffset: 2, rankOffset: 1 },
+          { fileOffset: -2, rankOffset: 1 },
+          { fileOffset: 1, rankOffset: -2 },
+          { fileOffset: -1, rankOffset: -2 },
+          { fileOffset: 2, rankOffset: -1 },
+          { fileOffset: -2, rankOffset: -1 },
+        ];
+    
+        for (const moveOffset of knightMoves) {
+          const targetFile = file.charCodeAt(0) - "A".charCodeAt(0) + moveOffset.fileOffset;
+          const targetRank = parseInt(rank) + moveOffset.rankOffset;
+    
+          if (targetFile >= 0 && targetFile <= 7 && targetRank >= 1 && targetRank <= 8) {
+            const newLocation : BoardLocation = (String.fromCharCode(targetFile + 65)+ targetRank.toString()) as BoardLocation
+            const pieceAtPosition : IPiece | undefined = BoardHelper.findPieceAtLocation(API,newLocation);
+    
+            if (!pieceAtPosition || pieceAtPosition.color !== this.color) {
+              // Add the valid move to the valid moves array
+              const move : Move = new Move(this,newLocation,API);
+              validMoves.push(move);
+            }
+          }
+        }
+    
+        return validMoves;
     }
 }
 
