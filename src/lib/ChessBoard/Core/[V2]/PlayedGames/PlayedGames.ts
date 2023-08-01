7676eb00bb48bb99ba4d35a90f0b667eb3eaee55
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-01 20:16:42 +0200

    added toJson and fromJson

--- src/lib/ChessBoard/Core/[V2]/PlayedGames/PlayedGames.ts
+++ src/lib/ChessBoard/Core/[V2]/PlayedGames/PlayedGames.ts
@@ -16,6 +16,25 @@ class PlayedGames implements IPlayedGames {
         this.Game_BlackPlayer = blackPlayer;
         this.Game_Outcome = outcome;
     }
+
+    // Convert the class instance to a plain object
+    public toJson() : string {
+        const object : IPlayedGames = {
+            Game_PlayedMoves: this.Game_PlayedMoves,
+            Game_ID: this.Game_ID,
+            Game_WhitePlayer: this.Game_WhitePlayer,
+            Game_BlackPlayer: this.Game_BlackPlayer,
+            Game_Outcome: this.Game_Outcome
+        }
+
+        return JSON.stringify(object);
+    }
+
+    // Factory method to create an instance from JSON data
+    static fromJson(data : IPlayedGames) : PlayedGames {
+        //const data : IPlayedGames = JSON.parse(jsonString);
+        return new PlayedGames(data.Game_PlayedMoves, data.Game_ID, data.Game_WhitePlayer, data.Game_BlackPlayer, data.Game_Outcome);
+    }
 }
 
 export default PlayedGames;
