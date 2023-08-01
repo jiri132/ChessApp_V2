ec3486c1ba7bfa32c5712d62b4700e168b0030fe
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-01 20:17:36 +0200

    Local storage update

--- src/lib/ChessBoard/Core/[V2]/ChessBoard.ts
+++ src/lib/ChessBoard/Core/[V2]/ChessBoard.ts
@@ -3,11 +3,14 @@ import Board from "./Board/Board";
 import type { IChessBoard } from "./Interfaces/IChessBoard";
 import PlayedGames from "./PlayedGames/PlayedGames";
 import BoardVisualHelper from "./Helpers/BoardVisualHelper";
+import { json } from "@sveltejs/kit";
+import type { IPlayedGames } from "./Interfaces/PlayedGames/IPlayedGames";
 
 class ChessBoard implements IChessBoard {
 
 
     public Board: Board = new Board();
+    
 
 
     public readonly PlayedGames : PlayedGames[] = [];
@@ -15,6 +18,8 @@ class ChessBoard implements IChessBoard {
 
 
     constructor() {
+        this.PlayedGames = this.GetPreviousGamesFromLocalStorage();
+        console.log(this.PlayedGames)
         this.newGame();
     }
 
@@ -25,11 +30,33 @@ class ChessBoard implements IChessBoard {
 
             //put everything into the list
             this.PlayedGames.push(playedGame);
+            this.SaveToLocalStorage();
         }
 
         // Create new board
-        BoardVisualHelper.RenderEmptyPlayedMovesContainer();
         this.Board = new Board();
+
+        BoardVisualHelper.RenderEmptyPlayedMovesContainer();
+        BoardVisualHelper.RenderAllTiles(this.Board);
+    }
+
+    private SaveToLocalStorage() : void {
+        localStorage.setItem("PlayedGames",JSON.stringify(this.PlayedGames));
+    }
+
+    private GetPreviousGamesFromLocalStorage() : PlayedGames[] {
+        const Games : PlayedGames[] = [];
+        const jsonString : string | null = localStorage.getItem("PlayedGames");
+
+        if (jsonString === "null" || jsonString === null) { console.log(Games); return []; }
+        
+        const object : IPlayedGames[] = JSON.parse(jsonString);
+
+        object.forEach((game_json : IPlayedGames) => {
+            Games.push(PlayedGames.fromJson(game_json));
+        });
+
+        return Games;
     }
 }
 
