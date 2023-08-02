4e4ab0043c9093004efd93f5483826c029093285
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-02 15:12:39 +0200

    added values

--- src/lib/ChessBoard/Core/[V2]/Types/Game/game.type.ts
+++ src/lib/ChessBoard/Core/[V2]/Types/Game/game.type.ts
@@ -1,5 +1,5 @@
 export enum GameType {
-    Human_VS_Human,
-    Human_VS_Bot,
-    Bot_VS_Bot
+    Human_VS_Human = "Human VS Human",
+    Human_VS_Bot = "Human VS Bot",
+    Bot_VS_Bot = "Bot VS Bot"
 }
